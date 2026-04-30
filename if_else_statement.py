91cec399ad23a5560ff3030277aa6433a4cc06eb
Savio Lovett <saviolovett@Savios-Laptop.local>
2026-04-30 01:21:17 -0400

    did some changes

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,11 +1,10 @@
-# lets have a simple code on if and else statment.
+"""Example program to compare numbers using an if-else statement."""
 
-a = 30
-b = 20
-c = 15
+A = 30
+B = 20
+C = 15
 
-if a > b and a > c:
-    print("a is larger than b")
+if A > B and A > C:
+    print("A is larger than B and C")
 else:
-    print("b is larger than a")
-
+    print("A is not the largest number")
