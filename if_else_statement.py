8621c4f7037b590838562ef0076b119f5e4fdee5
Savio Lovett <72256382+Lovett-hub@users.noreply.github.com>
2026-04-30 01:13:44 -0400

    Update if_else_statement.py

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,9 +1,11 @@
 # lets have a simple code on if and else statment.
 
-a = 10
+a = 30
 b = 20
+c = 15
 
-if a > b:
+if a > b and a > c:
     print("a is larger than b")
 else:
     print("b is larger than a")
+
